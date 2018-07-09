6d40fd37a3296fa33202f2a871e12ac5a894b42d
Clément Depraz <cdepraz@advanced-schema.com>
2018-07-09 19:25:43 +0200

    Update fake client behavior

--- server/client.c
+++ server/client.c
@@ -6,36 +6,30 @@
 // Pour avoir un client de test (ex: envoyer ping|null)
 // gcc client.c -o ./dist/softwar-client -lczmq -lzmq
 
-void clean(const char *buffer, FILE *fp)
-{
-    int c;
-    char *p = strchr(buffer, '\n');
-
-    if (p != NULL)
-        *p = 0;
-    else
-    {
-        while ((c = fgetc(fp)) != '\n' && c != EOF);
-    }
-}
-
 int main()
 {
-    printf("Connecting to server...\n");
-    char msg[20] = "";
+    printf("Connecting to server...");
+    char *msg;
     zsock_t *req = zsock_new(ZMQ_REQ);
-    zsock_connect(req, "tcp://localhost:8765");
-    int request_nbr;
-    do
+    if (zsock_connect(req, "tcp://localhost:8765") != 0) {
+        printf("FAIL\n");
+        return EXIT_FAILURE;
+    } else {
+        printf("SUCCESS...so far...\n");
+    }
+    
+    while(1)
     {
-        fgets(msg, sizeof(msg), stdin);
-        clean(msg, stdin);
+        scanf("%[^\n]%*c", msg);
+        if (strcmp(msg, "exit") == 0) {
+            break;
+        }
         zstr_sendf(req, "%s", msg);
 
         char *message = zstr_recv(req);
         printf("Received : %s\n", message);
         zstr_free(&message);
-    } while (strcmp(msg, "exit"));
+    }
 
     zsock_destroy(&req);
     return 0;
