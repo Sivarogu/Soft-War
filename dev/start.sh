c44c1d56443bc48460aeb913611f30b73c4040be
Pierre Tassel <ptassel@advanced-schema.com>
2018-08-30 20:02:01 +0200

    Improve dev, fix package-locks

--- dev/start.sh
+++ dev/start.sh
@@ -3,39 +3,37 @@ set -e pipefail
 
 choices=(
 	"build environment (do this first)"
-	"build and run server [watch]"
-	"build and run external/easy-event [watch]"
+	"build external/easy-event [watch]"
 	"build bridge [watch]"
-	"start bridge [watch]"
 	"build lib-client [watch]"
 	"build visualizer [watch]"
+	"build and run server [watch]"
+	"run bridge [watch]"
 )
 
 commands=(
 	"make -C dev build-env"
-	"make -C dev run-server"
-	"make -C dev run-easy-event"
-	"make -C dev run-bridge-build"
-	"make -C dev run-bridge-start"
-	"make -C dev run-lib-client"
-	"make -C dev run-visualizer"
+	"make -C dev build-easy-event"
+	"make -C dev build-bridge"
+	"make -C dev build-lib-client"
+	"make -C dev build-visualizer"
+	"make -C dev dev-server"
+	"make -C dev run-bridge"
 )
 
-ask_again=1
-while [ $ask_again -eq 1 ]; do
+while [ 1 ]; do
 	echo "Which script do you want to launch?"
 	choice_count=0
 	while [ "x${choices[choice_count]}" != "x" ]; do
-		echo "  "$(( $choice_count + 1 ))". ${choices[choice_count]}"
-		choice_count=$(( $choice_count + 1 ))
+		echo "  $choice_count. ${choices[choice_count]}"
+		choice_count=$(($choice_count + 1))
 	done
 	echo
 	read -p "> " user_choice
 
-	if [[ $user_choice < 1 || $user_choice > $choice_count ]]; then
+	if [[ $user_choice < 0 || $user_choice -ge $choice_count ]]; then
 		echo -e "\033[0;31minvalid choice: $user_choice\033[0m\n"
 	else
-		user_choice=$(( $user_choice - 1 ))
 		command="${commands[user_choice]}"
 		echo "execute command: $command"
 		eval $command
