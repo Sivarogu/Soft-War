ca36eb6d82c2f3cb0ef71d048affff752cd12f22
Clément Depraz <cdepraz@advanced-schema.com>
2018-08-30 23:13:55 +0200

    Allow to loop infinitly in server

--- server/src/thread_router.c
+++ server/src/thread_router.c
@@ -12,26 +12,34 @@ int thread_router_start() {
     zsock_bind(router, "tcp://*:%zu", global_config.rep_port);
     zsock_set_rcvtimeo(router, global_config.cycle_interval);
     log_debug("router socket bound on port %zu", global_config.rep_port);
-	log_debug("game is pending");
-    while (!zsys_interrupted && get_nb_player() < 4) {
-        message = request_receive(router);
-        if (message != NULL)
-            command_handle_lobby(router, message);
+    while (!zsys_interrupted) {
+        log_debug("game is pending");
+        pthread_mutex_lock(&game_info_mutex);
+        game_info.game_status = GAME_STATUS_PENDING;
+        pthread_mutex_unlock(&game_info_mutex);
+        while (!zsys_interrupted && get_nb_player() < 4)
+        {
+            message = request_receive(router);
+            if (message != NULL)
+                command_handle_lobby(router, message);
+        }
+        pthread_mutex_lock(&game_info_mutex);
+        pthread_cond_signal(&game_info_mutex_start_cond);
+        pthread_mutex_unlock(&game_info_mutex);
+        while (!zsys_interrupted && game_info.players && game_info.players->next)
+        {
+            message = request_receive(router);
+            if (message != NULL)
+                command_handle_game(router, message);
+        }
+        pthread_mutex_lock(&game_info_mutex);
+        game_info.game_status = GAME_STATUS_FINISHED;
+        pthread_cond_wait(&game_info_mutex_start_cond, &game_info_mutex);
+        pthread_mutex_unlock(&game_info_mutex);
     }
-    pthread_mutex_lock(&game_info_mutex);
-    game_info.game_status = GAME_STATUS_PENDING;
-    pthread_cond_signal(&game_info_mutex_start_cond);
-    pthread_mutex_unlock(&game_info_mutex);
-    while (!zsys_interrupted && game_info.players && game_info.players->next) {
-        message = request_receive(router);
-        if (message != NULL)
-            command_handle_game(router, message);
-    }
-    pthread_mutex_lock(&game_info_mutex);
-    game_info.game_status = GAME_STATUS_FINISHED;
-    pthread_mutex_unlock(&game_info_mutex);
     zsock_destroy(&router);
-	return 0;
+    BIND_NEG(log_debug("router socket destroyed"));
+    return 0;
 }
 
 void *thread_router_run() {
